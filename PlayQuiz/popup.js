221a6afb75bd4f09c0fc11e4380767db215d3e14
ashokkumarnaidu999@gmail.com <ashokkumarnaidu999@gmail.com>
2020-07-18 03:23:08 +0530

    adding options

--- PlayQuiz/popup.js
+++ PlayQuiz/popup.js
@@ -1,24 +1,12 @@
-// const options = document.querySelectorAll('input')
-var option1 = document.getElementById('option1')
-var option2 = document.getElementById('option2')
-var option3 = document.getElementById('option3')
-var option4 = document.getElementById('option4')
-
-
-
-const submit = document.getElementById('submit')
-submit.addEventListener('click', ()=> {
-    // console.log('enter')
-    // for(let i=0;i<options.length;i++) {
-        if(option1.value == 'on') {
-           console.log(option1.value, '1')
-        }else  if(option2.value == 'on') {
-            console.log(option2.value)
-         }else  if(option3.value == 'on') {
-            console.log(option3.value)
-         }else if(option4.value == 'on') {
-            console.log(option4.value)
-         }
-    // }
-    
-})
+console.log("asdf")
+function onSubmit() {
+   event.preventDefault()
+   var option = document.querySelectorAll("input");
+   var txt = "";
+   for (var i = 0; i <option.length; i++) {
+     if (option[i].checked) {
+       txt = txt + option[i].value + " ";
+     }
+   }
+   console.log(txt)
+  }
