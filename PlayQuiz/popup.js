5fd269979cd2857d959faeb154c56f7ac9c698f9
ashokkumarnaidu999@gmail.com <ashokkumarnaidu999@gmail.com>
2020-07-18 10:08:40 +0530

    loading options data

--- PlayQuiz/popup.js
+++ PlayQuiz/popup.js
@@ -10,3 +10,17 @@ function onSubmit() {
    }
    console.log(txt)
   }
+
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', '');
+
+xhr.responseType = 'json';
+
+xhr.send();
+
+// the response is {"message": "Hello, world!"}
+xhr.onload = function() {
+  let responseObj = xhr.response;
+  alert(responseObj.message); // Hello, world!
+};
